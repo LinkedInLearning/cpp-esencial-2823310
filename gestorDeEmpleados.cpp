58c4fc73a9c70ba0756e2378a831bffe742c86b4
Eliezer López <contacto@eliezerlopez.com>
2020-12-31 15:00:48 +0100

    Números enteros en C++

--- gestorDeEmpleados.cpp
+++ gestorDeEmpleados.cpp
@@ -2,9 +2,13 @@
  * Curso: C++ Esencial
  * Plataforma: LinkedIn Learning
  * Instructor: @EliezerLopez
- * */ 
+ * */
 
 int main(){
+
+    int edad;
+    edad = 18;
+    int cantidadDeBytes = sizeof(int);
 
     return 0;
 }
