9c04d62341aca4daf4742eaa3d9abbd39a45544e
Eliezer López <contacto@eliezerlopez.com>
2021-01-01 15:43:13 +0100

    Registros en C++

--- gestorDeEmpleados.cpp
+++ gestorDeEmpleados.cpp
@@ -4,7 +4,17 @@
  * Instructor: @EliezerLopez
  * */
 
+struct Empleado {
+
+    int identificador;
+    float salarioMensualBase;
+};
+
 int main(){
 
+    Empleado empleado1;
+    empleado1.identificador = 215;
+    empleado1.salarioMensualBase = 3000;
+    
     return 0;
 }
