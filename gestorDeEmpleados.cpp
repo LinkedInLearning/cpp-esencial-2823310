007e70eca357c1d93547a1cc82afc0c71816a4bc
Eliezer López <contacto@eliezerlopez.com>
2021-01-01 16:18:00 +0100

    Código base para explicar los vectores de alto nivel en C++

--- gestorDeEmpleados.cpp
+++ gestorDeEmpleados.cpp
@@ -3,16 +3,12 @@
  * Plataforma: LinkedIn Learning
  * Instructor: @EliezerLopez
  * */
-#include <string>
+#include <iostream>
+#include <vector>
+
 using namespace std;
 
 int main(){
-    
-    string holaMundo("Hola Mundo!");
-    string saludo;
 
-    char primeraLetra = holaMundo.at(0);
-    int longitudHolaMundo = holaMundo.length();
-    
     return 0;
 }
