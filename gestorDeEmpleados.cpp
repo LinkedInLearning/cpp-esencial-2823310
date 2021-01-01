98fed79001c51d86ca68c4ade9227b946a7612d3
Eliezer López <contacto@eliezerlopez.com>
2021-01-01 13:02:47 +0100

    Matrices en C++

--- gestorDeEmpleados.cpp
+++ gestorDeEmpleados.cpp
@@ -6,5 +6,11 @@
 
 int main(){
 
+    float ingresosMensualesEmpleados[5][12];
+
+    ingresosMensualesEmpleados[0][0] = 2300;
+    ingresosMensualesEmpleados[4][1] = 4800.33;
+    ingresosMensualesEmpleados[2][6] = 1200;
+    
     return 0;
 }
