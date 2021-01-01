674e579b1fa2aaf37a1131321c458dacbfc75539
Eliezer López <contacto@eliezerlopez.com>
2021-01-01 16:21:01 +0100

    Vectores de alto nivel en C++

--- gestorDeEmpleados.cpp
+++ gestorDeEmpleados.cpp
@@ -10,5 +10,14 @@ using namespace std;
 
 int main(){
 
+    vector<float> ingresosPrimerTrimestre;
+
+    ingresosPrimerTrimestre.push_back(1500);
+    ingresosPrimerTrimestre.push_back(1400);
+    ingresosPrimerTrimestre.push_back(1800);
+    
+    cout << ingresosPrimerTrimestre[0] << endl;
+    cout << ingresosPrimerTrimestre.at(1) << endl;
+    
     return 0;
 }
