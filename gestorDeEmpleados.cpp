25698f4f45828d721a6b252d9fd36678d6022409
Eliezer López <contacto@eliezerlopez.com>
2021-01-01 15:30:19 +0100

    Código base para explicar cadenas de caracteres de bajo nivel en C++

--- gestorDeEmpleados.cpp
+++ gestorDeEmpleados.cpp
@@ -5,12 +5,6 @@
  * */
 
 int main(){
-
-    float ingresosMensualesEmpleados[5][12];
-
-    ingresosMensualesEmpleados[0][0] = 2300;
-    ingresosMensualesEmpleados[4][1] = 4800.33;
-    ingresosMensualesEmpleados[2][6] = 1200;
-    
+ 
     return 0;
 }
