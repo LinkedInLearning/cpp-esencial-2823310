7a20d5f314fe16165b20ee2f4ee1077cb68fd87e
Eliezer López <contacto@eliezerlopez.com>
2021-01-01 15:58:07 +0100

    Cadenas de caracteres en C++

--- gestorDeEmpleados.cpp
+++ gestorDeEmpleados.cpp
@@ -3,8 +3,16 @@
  * Plataforma: LinkedIn Learning
  * Instructor: @EliezerLopez
  * */
+#include <string>
+using namespace std;
 
 int main(){
+    
+    string holaMundo("Hola Mundo!");
+    string saludo;
+
+    char primeraLetra = holaMundo.at(0);
+    int longitudHolaMundo = holaMundo.length();
     
     return 0;
 }
