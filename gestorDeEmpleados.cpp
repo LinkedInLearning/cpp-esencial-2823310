b6c9870d43dc7a7f6303a69404b2d0e4f3f494ad
Eliezer López <contacto@eliezerlopez.com>
2021-01-01 12:45:30 +0100

    Código base para explicar matrices en C++

--- gestorDeEmpleados.cpp
+++ gestorDeEmpleados.cpp
@@ -6,11 +6,5 @@
 
 int main(){
 
-    float ingresosPrimerTrimestre[3];
-
-    ingresosPrimerTrimestre[0] = 3200.70;
-    ingresosPrimerTrimestre[1] = 1300.20;
-    ingresosPrimerTrimestre[2] = 3700.73;
-
     return 0;
 }
