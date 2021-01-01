4a7a556c9d4f8184d88e7ccc184f32da7ee061c9
Eliezer López <contacto@eliezerlopez.com>
2021-01-01 12:43:12 +0100

    Vectores de bajo nivel en C++

--- gestorDeEmpleados.cpp
+++ gestorDeEmpleados.cpp
@@ -6,5 +6,11 @@
 
 int main(){
 
+    float ingresosPrimerTrimestre[3];
+
+    ingresosPrimerTrimestre[0] = 3200.70;
+    ingresosPrimerTrimestre[1] = 1300.20;
+    ingresosPrimerTrimestre[2] = 3700.73;
+
     return 0;
 }
