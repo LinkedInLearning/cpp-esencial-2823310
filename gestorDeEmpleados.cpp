ba1ead2384b7ea7496a88f276644cef434f8be6b
Eliezer López <contacto@eliezerlopez.com>
2021-01-01 16:23:55 +0100

    Salida por pantalla en C++

--- gestorDeEmpleados.cpp
+++ gestorDeEmpleados.cpp
@@ -3,21 +3,8 @@
  * Plataforma: LinkedIn Learning
  * Instructor: @EliezerLopez
  * */
-#include <iostream>
-#include <vector>
-
-using namespace std;
 
 int main(){
 
-    vector<float> ingresosPrimerTrimestre;
-
-    ingresosPrimerTrimestre.push_back(1500);
-    ingresosPrimerTrimestre.push_back(1400);
-    ingresosPrimerTrimestre.push_back(1800);
-    
-    cout << ingresosPrimerTrimestre[0] << endl;
-    cout << ingresosPrimerTrimestre.at(1) << endl;
-    
     return 0;
 }
