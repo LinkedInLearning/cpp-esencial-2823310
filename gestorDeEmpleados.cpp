6df04533328aead837b3bc2f8a81622b5074587b
Eliezer López <contacto@eliezerlopez.com>
2021-01-02 00:08:01 +0100

    Salida por pantalla en C++

--- gestorDeEmpleados.cpp
+++ gestorDeEmpleados.cpp
@@ -3,8 +3,21 @@
  * Plataforma: LinkedIn Learning
  * Instructor: @EliezerLopez
  * */
+#include <iostream>
+using namespace std;
 
 int main(){
 
+    cout << "Aprendiendo C++ con los cursos de LinkedIn Learning" << endl;
+
+    int numeroDeMiembrosPorEquipo = 7;
+    cout << "El número de miembros por equipo es de " << numeroDeMiembrosPorEquipo << " personas." << endl;
+
+    float pesoMedioEmpleados = 70.36;
+    cout << "El peso medio de los empleados es de " << pesoMedioEmpleados << " kg." << endl;
+
+    char primeraLetraNombre = 'E';
+    cout << "El candidato " << primeraLetraNombre << ". continúa el proceso de selección." << endl;
+    
     return 0;
 }
