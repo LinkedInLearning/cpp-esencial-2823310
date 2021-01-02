bb11b2f1a9deee0601730ff697c105b6239a70cb
Eliezer López <contacto@eliezerlopez.com>
2021-01-02 12:24:56 +0100

    Mostrando variables en pantalla para el proyecto global

--- gestorDeEmpleados.cpp
+++ gestorDeEmpleados.cpp
@@ -3,9 +3,17 @@
  * Plataforma: LinkedIn Learning
  * Instructor: @EliezerLopez
  * */
+#include <iostream>
+#include <string>
+using namespace std;
 
 int main(){
 
+    int identificador = 1500;
+    string nombre("Eliezer");
+    string apellido("López");
+
+    cout << "El identificador del empleado " << nombre << " " << apellido << " es el número " << identificador <<".";
 
     return 0;
 }
