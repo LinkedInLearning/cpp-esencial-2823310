057492d9efe58a4146c6dd9eee6bb96ee2cfb117
Eliezer López <contacto@eliezerlopez.com>
2021-01-03 18:23:06 +0100

    Uso de punteros en C++

--- punteros.cpp
+++ punteros.cpp
@@ -11,8 +11,19 @@ int main(){
     int identificador = 20;
     int *p_identificador = &identificador;
 
+    int identificador2 = 40;
+    int *p_identificador2 = &identificador2;
+
+    int *p_asignacion;
+    p_asignacion = p_identificador;
+
     cout << "Contenido de identificador: " << identificador << endl;
     cout << "Contenido de p_identificador: " << p_identificador << endl;
+
+    cout << "Contenido de identificador2: " << identificador2 << endl;
+    cout << "Contenido de p_identificador2: " << p_identificador2 << endl;
+
+    cout << "Contenido de p_asignacion: " << p_asignacion << endl;
 
     return 0;
 } 
