4d8a8231a100547945f71e9a7bc3ddb2fbfef0e1
Eliezer López <contacto@eliezerlopez.com>
2021-01-03 18:28:35 +0100

    Operaciones aritméticas en C++

--- operacionesAritmeticas.cpp
+++ operacionesAritmeticas.cpp
@@ -6,5 +6,12 @@
 
 int main(){
 
+    int resultadoSuma = 1 + 9;
+    float resultadoResta = 7.5 - 4;
+    float tresDocenas = 3 * 12;
+
+    float cociente = 10 / 3.0;
+    int resto = 10 % 3;
+    
     return 0;
 }
