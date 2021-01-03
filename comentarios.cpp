f1476f888c1bc22d3392f102d8335673fe40e695
Eliezer López <contacto@eliezerlopez.com>
2021-01-03 17:53:44 +0100

    Código para explicar comentarios en C++

---
+++ comentarios.cpp
@@ -0,0 +1,18 @@
+/*
+ * Curso: C++ Esencial
+ * Plataforma: LinkedIn Learning
+ * Instructor: @EliezerLopez
+ * */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout << "Aprendiendo a programar en C++";
+    cout << "con los cursos de LinkedIn Learning";
+
+    // Las palabras 'C++' y 'con' se imprimirán en pantalla sin empacio entre ellas
+    
+    return 0;
+}
