3787387ffaa1aef18c502ad9489cfeb060444409
Eliezer López <contacto@eliezerlopez.com>
2021-01-03 18:08:57 +0100

    Declaración e inicialización de punteros en C++

--- punteros.cpp
+++ punteros.cpp
@@ -3,10 +3,16 @@
  * Plataforma: LinkedIn Learning
  * Instructor: @EliezerLopez
  * */
+#include <iostream>
+using namespace std;
 
 int main(){
 
-    
+    int identificador = 20;
+    int *p_identificador = &identificador;
+
+    cout << "Contenido de identificador: " << identificador << endl;
+    cout << "Contenido de p_identificador: " << p_identificador << endl;
 
     return 0;
 } 
