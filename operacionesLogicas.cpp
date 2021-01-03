ac0d215876e081b9997946a338dc82a9d39bb6d9
Eliezer López <contacto@eliezerlopez.com>
2021-01-03 18:46:33 +0100

    Operaciones lógicas en C++

--- operacionesLogicas.cpp
+++ operacionesLogicas.cpp
@@ -6,6 +6,13 @@
 
 int main(){
 
+    bool resultadoLogico1 = 5.8 < 10;
+    bool resultadoLogico2 = 16 > 16;
+    bool resultadoLogico3 = 'e' != 'E';
 
+    bool resultadoAND = resultadoLogico1 && resultadoLogico2;
+    bool resultadoOR = resultadoLogico1 || resultadoLogico2;
+    bool resultadoNOT = !resultadoLogico3;
+    
     return 0;
 }
