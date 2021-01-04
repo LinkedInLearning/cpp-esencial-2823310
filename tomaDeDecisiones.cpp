455404dd7712454cc33c36aa7d7c4cdf00ef2e2c
Eliezer López <contacto@eliezerlopez.com>
2021-01-04 22:07:18 +0100

    Estructura SWITCH en C++

--- tomaDeDecisiones.cpp
+++ tomaDeDecisiones.cpp
@@ -3,8 +3,22 @@
  * Plataforma: LinkedIn Learning
  * Instructor: @EliezerLopez
  * */
+#include <iostream>
 
 int main(){    
+
+    int puntuacionExamen = 8;
+
+    switch (puntuacionExamen){
+        case 0: std::cout << "Suspenso"; break;
+        case 1: std::cout << "Suspenso"; break;
+        case 2: std::cout << "Suspenso"; break;
+        case 3: std::cout << "Suspenso"; break;
+        case 4: std::cout << "Suspenso"; break;
+        case 5: std::cout << "Suspenso"; break;
+        case 10: std::cout << "Suspenso"; break;
+        default: std::cout << "Puntuación no contemplada."; break;
+    }
 
     return 0;
 }
