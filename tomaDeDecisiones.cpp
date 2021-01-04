1d87559ff4e98a77e8b1b98fa736f00f33a0d834
Eliezer López <contacto@eliezerlopez.com>
2021-01-04 21:52:58 +0100

    Estructura IF-ELSE en C++

--- tomaDeDecisiones.cpp
+++ tomaDeDecisiones.cpp
@@ -3,8 +3,23 @@
  * Plataforma: LinkedIn Learning
  * Instructor: @EliezerLopez
  * */
+#include <iostream>
+using namespace std;
 
 int main(){
+
+    bool usuarioTieneSanciones;
+    bool usuarioTuvoSancionesEnElPasado;
+    
+    if (usuarioTieneSanciones){
+        cout << "Lo sentimos, pero el usuario tiene sanciones pendientes." << endl;
+    }
+    else if(usuarioTuvoSancionesEnElPasado) {
+        cout << "Lo sentimos, pero el usuario tuvo sanciones pendientes en el pasado." << endl;
+    }
+    else {
+        cout << "Usuario libre de sanciones. ¡Genial!";
+    }
 
     return 0;
 }
