60e7b0281cc45827f7d2e3fefd0d56ddd4f9774d
Eliezer López <contacto@eliezerlopez.com>
2021-01-06 17:14:52 +0100

    Parámetros predeterminados de una función en C++

--- funcion.cpp
+++ funcion.cpp
@@ -4,16 +4,16 @@
  * Instructor: @EliezerLopez
  * */
 
-float calculaIvaEnEuros(float);
+float calculaIvaEnEuros(float, int, float);
 
 int main(){
 
     float importeTotal = 1500.34;
-    float ivaCalculadoEnEuros = calculaIvaEnEuros(importeTotal);
+    float ivaCalculadoEnEuros = calculaIvaEnEuros(importeTotal, 12, 13.4);
     
     return 0;
 }
 
-float calculaIvaEnEuros(float importeTotalEnEuros){
+float calculaIvaEnEuros(float importeTotalEnEuros, int a, float b){
     return (importeTotalEnEuros * 0.21);
 }
